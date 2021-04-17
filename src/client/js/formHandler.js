c15924907ce865d9781acb6495ffb5d269b9ef72
MostafaOthman12 <Mostafayoussef82@gmail.com>
2021-04-17 05:30:55 +0200

    Project 1

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,12 +1,9 @@
 export async function handleSubmit(event) {
 	event.preventDefault();
 	let response;
-	let Regex = '^[a-zA-Z0-9]*$';
 	// check what text was put into the form field
 	let formText = document.getElementById('name').value;
-	formText.match(Regex)
-		? (response = await Client.MeaningAPI(formText))
-		: alert('Only Letters and Numbers ');
+	response = await Client.MeaningAPI(formText);
 
 	document.getElementById('results').innerHTML = `<ul>
 	                                    <li>${response.model} </li>
