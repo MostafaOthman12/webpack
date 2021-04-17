baeaa0b55073c4d95962046ea65d2ef96ffd9ec3
MostafaOthman12 <Mostafayoussef82@gmail.com>
2021-04-17 15:34:15 +0200

    Project1

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,13 +2,14 @@ export async function handleSubmit(event) {
 	event.preventDefault();
 	let response;
 	// check what text was put into the form field
-	let formText = document.getElementById('name').value;
-	response = await Client.MeaningAPI(formText);
+	let formText = document.getElementById('name').value
+		? (response = await Client.MeaningAPI(formText))
+		: alert('Cant be null');
 
 	document.getElementById('results').innerHTML = `<ul>
 	                                    <li>${response.model} </li>
 										<li>${response.irony} </li>
 										<li>${response.agreement} </li>
-										<li>${response.confidence} </li>
+						 				<li>${response.confidence} </li>
 										`;
 }
